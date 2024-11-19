f9cc5cf55db45a9562969b1af59c54f51e3ee6df
bvfr <67278922+bvfr@users.noreply.github.com>
2024-11-19 13:58:37 -0500

    Update ProcessManager.cpp

--- ProcessManager.cpp
+++ ProcessManager.cpp
@@ -9,6 +9,11 @@ ProcessManager::ProcessManager() : currentPage(0) {
     loadProcesses();
 }
 
+// Get all processes
+const std::vector<Process>& ProcessManager::getProcesses() const {
+    return processes;
+}
+
 // Load all processes from the system
 void ProcessManager::loadProcesses() {
     processes.clear();
@@ -32,82 +37,34 @@ void ProcessManager::loadProcesses() {
         }
     }
     closedir(procDir);
-}
-
-// Refresh all processes (update resource usage)
-void ProcessManager::refreshProcesses() {
+    
+    // Update resource usage for all processes
     for (auto& process : processes) {
         process.updateUsage();
-    }
-}
-
-// Display 10 processes per page
-void ProcessManager::displayProcesses() const {
-    size_t startIndex = currentPage * pageSize;
-    size_t endIndex = std::min(startIndex + pageSize, processes.size());
-
-    for (size_t i = startIndex; i < endIndex; ++i) {
-        processes[i].display();
     }
 }
 
 // Sort processes based on user-selected criteria
 void ProcessManager::sortProcesses(const std::string& criteria) {
     if (criteria == "cpu") {
-        sortByCpuUsage();
+        std::sort(processes.begin(), processes.end(),
+                 [](const Process& a, const Process& b) {
+                     return a.getCpuUsage() > b.getCpuUsage();
+                 });
     } else if (criteria == "memory") {
-        sortByMemoryUsage();
+        std::sort(processes.begin(), processes.end(),
+                 [](const Process& a, const Process& b) {
+                     return a.getMemoryUsage() > b.getMemoryUsage();
+                 });
     } else if (criteria == "disk") {
-        sortByDiskUsage();
+        std::sort(processes.begin(), processes.end(),
+                 [](const Process& a, const Process& b) {
+                     return a.getDiskUsage() > b.getDiskUsage();
+                 });
     } else if (criteria == "network") {
-        sortByNetworkUsage();
+        std::sort(processes.begin(), processes.end(),
+                 [](const Process& a, const Process& b) {
+                     return a.getNetworkUsage() > b.getNetworkUsage();
+                 });
     }
-}
-
-// Pagination - Show next page
-void ProcessManager::showNextPage() {
-    if ((currentPage + 1) * pageSize < processes.size()) {
-        currentPage++;
-    }
-}
-
-// Pagination - Show previous page
-void ProcessManager::showPreviousPage() {
-    if (currentPage > 0) {
-        currentPage--;
-    }
-}
-
-// Get all processes
-const std::vector<Process>& ProcessManager::getProcesses() const {
-    return processes;
-}
-
-// Sorting methods
-void ProcessManager::sortByCpuUsage() {
-    std::sort(processes.begin(), processes.end(),
-              [](const Process& a, const Process& b) {
-                  return a.getCpuUsage() > b.getCpuUsage();
-              });
-}
-
-void ProcessManager::sortByMemoryUsage() {
-    std::sort(processes.begin(), processes.end(),
-              [](const Process& a, const Process& b) {
-                  return a.getMemoryUsage() > b.getMemoryUsage();
-              });
-}
-
-void ProcessManager::sortByDiskUsage() {
-    std::sort(processes.begin(), processes.end(),
-              [](const Process& a, const Process& b) {
-                  return a.getDiskUsage() > b.getDiskUsage();
-              });
-}
-
-void ProcessManager::sortByNetworkUsage() {
-    std::sort(processes.begin(), processes.end(),
-              [](const Process& a, const Process& b) {
-                  return a.getNetworkUsage() > b.getNetworkUsage();
-              });
 }
